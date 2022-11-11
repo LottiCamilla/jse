9d2431da8c83054d3d297d1f7216e266ea9c9d4f
Camilla Lotti <camilla.lotti01@universitadipavia.it>
2022-11-11 16:23:25 +0100

    Completed exercise m01 s09

--- src/main/java/com/example/jse/m01/s09/Exercise.java
+++ src/main/java/com/example/jse/m01/s09/Exercise.java
@@ -2,10 +2,13 @@ package com.example.jse.m01.s09;
 
 public class Exercise {
     public static void main(String[] args) {
-        // TODO
         // (1) create an array with "one", "two", "three", "four" in it
         // (2) print the number of its elements
         // (3) print the last element
+    	
+    	String[] data = {"one", "two", "three", "four"};
+    	System.out.println("L'array ha un lunghezza pari a " + data.length);
+    	System.out.println("L'ultimo valore è " + data[data.length - 1]);
 
         System.out.println("Done");
     }
