5bc4f9257aa2e718248c0570b4b58ba1a0398a8b
Camilla Lotti <camilla.lotti01@universitadipavia.it>
2022-11-11 17:00:30 +0100

    Completed MatrixArraysExercise m01 s10

--- src/main/java/com/example/jse/m01/s10/MatrixArraysExercise.java
+++ src/main/java/com/example/jse/m01/s10/MatrixArraysExercise.java
@@ -1,14 +1,21 @@
 package com.example.jse.m01.s10;
 
+import java.util.Arrays;
+
 public class MatrixArraysExercise {
     public static void main(String[] args) {
-        // TODO:
         // (1) Create a two line matrix
         // Row 1: "ciao", "buongiorno"
         // Row 2: "hello", "good morning"
         // (2) Print the first English greeting
         // (3) Convert the Italian greetings in a string and print it
         // (4) Convert the full matrix in a string and print it
+    	
+    	String[][] matrix = {{"ciao", "buongiorno"}, {"hello", "good morning"}};
+    	
+    	System.out.println("(2): " + matrix[1][0]);
+    	System.out.println("(3): " + Arrays.toString(matrix[0]));
+    	System.out.println("(4): " + Arrays.deepToString(matrix));
 
         System.out.println("Done");
     }
