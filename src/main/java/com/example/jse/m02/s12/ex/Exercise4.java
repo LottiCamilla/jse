d6a6a830906a4021bf248db09b533954cd3f51f6
Camilla Lotti <camilla.lotti01@universitadipavia.it>
2022-11-14 16:27:10 +0100

    Added switch - Exercise4 m01 s12

--- src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
+++ src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
@@ -3,7 +3,7 @@ package com.example.jse.m02.s12.ex;
 public class Exercise4 {
 	public static void main(String[] args) {
 		/*
-		 * TODO: print up to 3 args
+		 * Print up to 3 args
 		 * 
 		 * ["ciao", "hello"] -> "ciao" "hello"
 		 * 
@@ -14,5 +14,21 @@ public class Exercise4 {
 		for (int i = 0; i < len; i++) {
 			System.out.println("Argomento " + i + ": " + args[i]);
 		}
+		
+//		switch (args.length) {
+//		default:
+//			// FALL THROUGH
+//		case 3:
+//			System.out.println(args[2]);
+//			// FALL THROUGH
+//		case 2:
+//			System.out.println(args[1]);
+//			// FALL THROUGH
+//		case 1:
+//			System.out.println(args[0]);
+//			// FALL THROUGH
+//		case 0: 
+//			break;
+//		}
 	}
 }
