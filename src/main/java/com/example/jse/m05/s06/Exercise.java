c14013eab90d75365ee7f6006127b212ee8ea714
Camilla Lotti <camilla.lotti01@universitadipavia.it>
2022-11-16 14:25:00 +0100

    Completed Exercise m05 s06

--- src/main/java/com/example/jse/m05/s06/Exercise.java
+++ src/main/java/com/example/jse/m05/s06/Exercise.java
@@ -1,14 +1,26 @@
 package com.example.jse.m05.s06;
 
 public class Exercise {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Pass me one argument!");
-        } else {
-            // TODO (1) check if the argument contains only ciphers
-            // TODO (2) check if the argument contains at least one cipher
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Pass me one argument!");
+		} else {
+			// (1) check if the argument contains only ciphers
+			// (2) check if the argument contains at least one cipher
 
-            System.out.println("Done");
-        }
-    }
+			System.out.println("args[0] = " + args[0]);
+			boolean check1 = true;
+			boolean check2 = false;
+			for (char c : args[0].toCharArray()) {
+				if (Character.isDigit(c)) {
+					check2 = true;
+				} else {
+					check1 = false;
+				}
+			}
+
+			System.out.println("(1) the argument contains only ciphers: \t" + check1);
+			System.out.println("(2) the argument contains at least one cipher: \t" + check2);
+		}
+	}
 }
